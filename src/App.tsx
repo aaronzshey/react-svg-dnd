7f923231c168f47746645f82b8fa12e3571b6a25
Aaron.ZhiRen.Shey <aaron.z.shey@proton.me>
2024-12-18 17:23:28 -0800

    added a feature

--- src/App.tsx
+++ src/App.tsx
@@ -8,8 +8,25 @@ function Circle() {
 
   function handleMouseMove(e: MouseEvent) {
     setPosition(position => {
-      const xDiff = position.coords.x - e.pageX
-      const yDiff = position.coords.y - e.pageY
+      // from the MDN web docs, pageX and pageY are used to access the 
+      // x and y coords of the mouse
+      // https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/pageX
+      // https://developer.mozilla.org/en-US/docs/Web/API/MouseEvent/pageY
+
+      const mouseX = e.pageX;
+      const mouseY = e.pageY;
+
+
+      // by calculating the difference between the current position of the mouse,
+      // we can move the circle by clicking anywhere on the circle.
+      // without this code, clicking on the circle will make it "jump" to the position
+      // of the mouse, which looks weird.
+      const xDiff = position.coords.x - mouseX
+      const yDiff = position.coords.y - mouseY
+
+      // the coords field stores the current position of the mouse.  
+      // without the coords field, the circle will also jump to the position 
+      // of the mouse when the mouse is moved
       return {
         x: position.x - xDiff,
         y: position.y - yDiff,
@@ -21,51 +38,74 @@ function Circle() {
     })
   }
 
+  // we need a ref to the function so we can move the SVG without re-rendering
+  // we have to write handleMouseMove as unknown as EventListener because typescript doesn't
+  // treat Events and MouseEvents the same way.
   const handleMouseMoveRef = useRef<EventListener>(handleMouseMove as unknown as EventListener)
 
+
+  // when the mouse is clicked, we change the coords field to the current mouse position,
+  // so the circle moves accurately, but we don't move anything else.
+
   function handleMouseDown(e: MouseEvent<SVGElement>) {
-    const pageX = e.pageX
-    const pageY = e.pageY
+    const mouseX = e.pageX
+    const mouseY = e.pageY
     setPosition(position =>
       Object.assign({}, position, {
         coords: {
-          x: pageX,
-          y: pageY,
+          x: mouseX,
+          y: mouseY,
         },
       }),
     )
     document.addEventListener('mousemove', handleMouseMoveRef.current)
   }
 
+
   function handleMouseUp() {
+    // stop dragging on mouse up
     document.removeEventListener('mousemove', handleMouseMoveRef.current)
+
+    // removing this line doesn't actually change anything
+    // I assume it's to cover some edge case, so I'll leave it be
     setPosition(position => Object.assign({}, position, { coords: {} }))
   }
 
-  return (
-    <circle
-      cx={position.x}
-      cy={position.y}
-      r={25}
-      fill="black"
-      stroke="black"
-      strokeWidth="1"
-      onMouseDown={handleMouseDown}
-      onMouseUp={handleMouseUp}
-    />
-  )
-}
+  // here's some new code that makes the circle stop moving when your cursor leaves 
+  // the canvas.  I also put in the set position code just in case.  
 
-export default function App() {
+  function handleMouseLeave() {
+    document.removeEventListener('mousemove', handleMouseMoveRef.current);
+    setPosition(position => Object.assign({}, position, { coords: {} }))
+  }
   return (
+
     <svg
       style={{
         border: '1px solid green',
         height: '200px',
         width: '100%',
       }}
+
+      onMouseLeave={handleMouseLeave}
     >
-      <Circle />
+      <circle
+        cx={position.x}
+        cy={position.y}
+        r={25}
+        fill="white"
+        strokeWidth="1"
+        onMouseDown={handleMouseDown}
+        onMouseUp={handleMouseUp}
+      />
+
     </svg>
+  )
+}
+
+
+export default function App() {
+  return (
+    <Circle />
   )
 }
